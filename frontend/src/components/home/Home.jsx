522ed0de16f647ceeb010e41807a76dfcaca65f1
janaina maria <janainamariambm@gmail.com>
2022-06-10 10:42:12 -0300

    fim da aplicação

---
+++ frontend/src/components/home/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Main from '../template/Main'
+
+export default props => (
+  <Main icon="home" title="inicio" subtitle="Sistema de cadastro">
+    <div className="display-4">Bem Vindo!</div>
+
+    <p className="mb-0">Ao sistema da nossa açaiteria!</p>
+  </Main>
+)
---
+++ frontend/src/components/template/Footer.jsx
@@ -0,0 +1,13 @@
+import './Footer.css'
+import React from 'react'
+
+export default props => (
+  <footer className="footer">
+    <span>
+      Desenvolvido por <i className="fa fa-heart text-danger"></i>
+      <strong>
+        <span className="text-danger"> Janaina Maria</span>
+      </strong>
+    </span>
+  </footer>
+)
